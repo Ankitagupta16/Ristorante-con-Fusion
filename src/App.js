65731cf838bddc257260699b6c198a479b5f92ca
ankita gupta <1610ankita@gmail.com>
2020-04-19 07:48:51 +0530

    first assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import logo from '.logo.svg';
-import Menu from './components/menuComponent';
+import Menu from './components/MenuComponent';
 import { Navbar,NavbarBrand } from 'reactstrap';
 import './App.css';
 import { DISHES } from './shared/dishes';
