28eeeea7f78ba3ac12ed0d31eb6923e8765911e4
ankita gupta <1610ankita.gupta@gmail.com>
2020-04-22 09:31:12 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -50,6 +51,7 @@ class Main extends Component{
 					<Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
 					<Route path='/menu/:dishId' component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
+					<Route path="/aboutus" component={() => <About leaders ={this.state.leaders}/>} />
 					<Redirect to="/home" />
 				</Switch>
 			<Footer />
